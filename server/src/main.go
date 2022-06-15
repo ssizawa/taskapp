5bdbd132d9691a83d5f03d39bc4482943e2b72e2
ssizawa <izawa.shin@mikilab.doshisha.ac.jp>
2022-06-16 02:31:01 +0900

    db.go作成

--- server/src/main.go
+++ server/src/main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
-	//HTTPクライアントとサーバーの実装
-	//"net/http"
-
 	//Ginの実装
-	//"github.com/gin-gonic/gin"
-
-	"github.com/ssizawa/taskapp/server/src/repository"
+	"github.com/gin-gonic/gin"
+	//"github.com/ssizawa/taskapp/server/src/repository"
 )
 
 func main() {
-	repository.Create_table()
+	//repository.Opendb()
 
-	// //Ginの変数
-	// router := gin.Default()
+	//Ginの変数
+	router := gin.Default()
 
-	// //html/css/jsファイルのディレクトリをロード
-	// router.LoadHTMLGlob("templates/*")
-	// router.Static("/assets", "./assets")
+	//html/css/jsファイルのディレクトリをロード
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/assets", "./assets")
 
-	// //GET
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "login.html", gin.H{})
-	// })
-
-	// //POSTのデータ
-	// router.POST("/TaskApp", func(c *gin.Context) {
-
-	// 	name := c.PostForm("name")
-	// 	password := c.PostForm("password")
-
-	// 	fmt.Print(name, password)
-	// })
-
-	// //ポートを指定して実行
-	// router.Run(":3000")
+	//ポートを指定して実行
+	router.Run(":3000")
 }
