39881d0f5d1fcb53f3934b7bd42176a48371b6ff
Konippi <konippi-114@outlook.jp>
2022-06-17 12:04:03 +0900

    completed sidemenu

--- server/src/assets/js/index.js
+++ server/src/assets/js/index.js
@@ -1,11 +1,23 @@
 $(function(){
-    $('#right').on('click', function(){
-        $('.sidemenu').animate({
-            'display': 'block' 
-        }).animate({
-            'left': 0
-        },
-        300
-        );
-    });
+
+  var duration = 300;
+  var $trriger = $('#trigger');
+  var $sidemenu = $('#sidemenu');
+  var $trriger_close = $('#trigger-close');
+
+  $trriger.on('click', function(){
+    if(!$trriger_close.hasClass('open')){
+        $sidemenu.stop(true).animate({
+          left: 0
+        }, duration);
+        $trriger_close.addClass('open');
+    }
+  });
+
+  $trriger_close.on('click', function(){
+    $sidemenu.stop(true).animate({
+      left: '-300px'
+    }, duration);
+    $(this).removeClass('open');
+  });
 });
